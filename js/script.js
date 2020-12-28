e069ce5e3f54a5f5d6779f5b9b616f55115fac30
FariaParvinMegha <sk15chapainawabganj@gmail.com>
2020-12-29 03:27:13 +0600

    homepage-final-commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 
-$(document).ready(function () {
+$(function () {
+
     // init flickity
     $('.pic-slider').flickity({
         // options
@@ -10,11 +11,12 @@ $(document).ready(function () {
         groupCells: true,
         wrapAround: true,
         draggable: true,
+        percentPosition: false,
+        adaptiveHeight: true,
         cellAlign: "left",
         arrowShape: "M95.849,46.323H14.1L40.364,20.15a4.166,4.166,0,0,0-5.9-5.881L1.076,47.537a4.162,4.162,0,0,0,0,5.891L34.462,86.7a4.166,4.166,0,0,0,5.9-5.881L14.1,54.642H95.849A4.159,4.159,0,1,0,95.849,46.323Z",
         pauseAutoPlayOnHover: false
     });
-
 
 
     // init scroll-magic
@@ -41,4 +43,14 @@ $(document).ready(function () {
     })
         .setClassToggle('#back2top', 'fade-in')
         .addTo(controller);
+
+    //show mobile nav
+    $('#mobile-nav-open-btn').click(function(){
+        $('#mobile-nav').css("height", "100%");
+    });
+    //hide mobile nav
+    $('#mobile-nav-close-btn, #mobile-nav a').click(function(){
+        $('#mobile-nav').css("height", "0");
+    });
+
 });
